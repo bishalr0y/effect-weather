600ea2968f436e7fd4f0b17af7b477b3f7243aec
bishalr0y <bishalroy895@gmail.com>
2025-12-02 11:49:28 +0530

    add weather response interface

--- pad.ts
+++ pad.ts
@@ -1,13 +1,43 @@
-const getWeather = async (location: string) => {
+interface WeatherResponse {
+  name: string;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sys: {
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+const getWeather = async (location: string): Promise<WeatherResponse> => {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_API_KEY}`,
     );
-    const json = await response.json();
-    console.log(json);
+    return (await response.json()) as Promise<WeatherResponse>;
   } catch (error) {
-    console.log(error);
+    console.log(`Error: ${error}`);
+    throw new Error();
   }
 };
 
-getWeather("guwahati");
+const data = await getWeather("guwahati");
+console.log(data.weather);
