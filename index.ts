cec31f963bb24e86c1f0d08c9c5aec5ef48e09b3
bishalr0y <bishalroy895@gmail.com>
2025-12-02 12:05:16 +0530

    wip

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
-import { Command, Prompt } from "@effect/cli";
+import { Prompt } from "@effect/cli";
 import { NodeContext, NodeRuntime } from "@effect/platform-node";
 import { Console, Effect } from "effect";
 
-const command = Command.make("weather", {}, () =>
-  Effect.gen(function* () {
-    const location = yield* Prompt.text({ message: "Enter your location: " });
-    Console.log(location);
-  }),
-);
-
-const cli = Command.run(command, {
-  name: "effect-weather",
-  version: "v0.0.1",
+const main = Effect.gen(function* () {
+  while (true) {
+    const input = yield* Prompt.text({ message: "Enter your location: " });
+    Console.log(input);
+  }
 });
 
-cli(process.argv).pipe(Effect.provide(NodeContext.layer), NodeRuntime.runMain);
+main.pipe(Effect.provide(NodeContext.layer), NodeRuntime.runMain);
