fdfe1c62b84fe5a1277c75c0442f63e5b58ae1a4
bishalr0y <bishalroy895@gmail.com>
2025-12-03 20:03:40 +0530

    api call working wip

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
 import { Prompt } from "@effect/cli";
 import { NodeContext, NodeRuntime } from "@effect/platform-node";
-import { Console, Effect } from "effect";
+import { Console, Effect, Schema, Config, Redacted } from "effect";
+import { FetchHttpClient, HttpClient } from "@effect/platform";
+import { Weather } from "./schema";
 
-const main = Effect.gen(function* () {
-  while (true) {
-    const input = yield* Prompt.text({ message: "Enter your location: " });
-    Console.log(input);
-  }
+const getWeather = Effect.fn(function* (location: string) {
+  //         ^?
+  const apiKey = yield* Config.redacted("OPENWEATHER_API_KEY");
+
+  const response = yield* HttpClient.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${Redacted.value(apiKey)}`,
+  );
+
+  const json = yield* response.json;
+  return yield* Schema.decodeUnknown(Weather)(json);
 });
 
-main.pipe(Effect.provide(NodeContext.layer), NodeRuntime.runMain);
+const main = Effect.gen(function* () {
+  const input = yield* Prompt.text({ message: "Enter your location: " });
+  const weather = yield* getWeather(input);
+  yield* Console.log(weather);
+});
+
+main.pipe(
+  Effect.provide(NodeContext.layer),
+  Effect.provide(FetchHttpClient.layer),
+  NodeRuntime.runMain,
+);
